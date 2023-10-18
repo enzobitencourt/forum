779fd1ab6b7693025ddd5b697d05b818f295fbaa
enzobitencourt <enzogbitencourt@gmail.com>
2023-10-17 22:44:10 -0300

    bd partes

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -38,47 +38,54 @@ async function listUsers(request, response) {
 
 // Função que cria um novo usuário 
 async function storeUser(request, response) {
-    // Preparar o comando de execução no banco
-    const query = 'INSERT INTO usuarios(nome, email, senha) VALUES(?, ?, ?);';
-
-    // Recuperar os dados enviados na requisição
-    const params = Array(
-        request.body.nome,
+    const values = [
+        request.body.firstName,
+        request.body.sobrenome,
+        request.body.telefone,
         request.body.email,
         bcrypt.hashSync(request.body.senha, 10)
-    );
+    ];
 
-    // Executa a ação no banco e valida os retornos para o client que realizou a solicitação
-    connection.query(query, params, (err, results) => {
+    // Use placeholders na consulta SQL
+    const query = "INSERT INTO usuarios (nome, sobrenome, telefone, email, senha) VALUES (?, ?, ?, ?, ?)";
+
+    // Execute a ação no banco de dados e valide os retornos para o cliente que realizou a solicitação
+    connection.query(query, values, (err, results) => {
         try {
-            if (results) {
-                response
-                    .status(201)
-                    .json({
-                        success: true,
-                        message: `Sucesso! Usuário cadastrado.`,
-                        data: results
-                    });
-            } else {
-                response
-                    .status(400)
-                    .json({
+            if (err) {
+                if (err.code === 'ER_DUP_ENTRY') { // Verifica o código de erro para violação de chave única
+                    response.status(400).json({
                         success: false,
-                        message: `Não foi possível realizar o cadastro. Verifique os dados informados`,
-                        query: err.sql,
+                        message: "O email já existe. Escolha outro email.",
+                        query: query,
                         sqlMessage: err.sqlMessage
                     });
-            }
-        } catch (e) { // Caso aconteça algum erro na execução
-            response.status(400).json({
-                    succes: false,
-                    message: "Ocorreu um erro. Não foi possível cadastrar usuário!",
-                    query: err.sql,
-                    sqlMessage: err.sqlMessage
+                } else {
+                    response.status(400).json({
+                        success: false,
+                        message: "Não foi possível realizar o cadastro. Verifique os dados informados",
+                        query: query,
+                        sqlMessage: err.sqlMessage
+                    });
+                }
+            } else {
+                response.status(201).json({
+                    success: true,
+                    message: "Sucesso! Usuário cadastrado.",
+                    data: results
                 });
+            }
+        } catch (e) {
+            response.status(400).json({
+                success: false,
+                message: "Ocorreu um erro. Não foi possível cadastrar o usuário!",
+                query: query,
+                sqlMessage: err.sqlMessage
+            });
         }
     });
 }
+
 
 // Função que atualiza o usuário no banco
 async function updateUser(request, response) {
--- frontend/src/Pages/Cadastro/Cadastro.js
+++ frontend/src/Pages/Cadastro/Cadastro.js
@@ -1,112 +1,154 @@
-import { Container, Form, FormImage, Image, InputBox, Input, Label, ContainerForm, Divi, LogoImg, ContainerFormulario, StyleForm, Botao, InputBoxContainer } from "./styled"
-import ImagemPrincipal from "../../Assets/imageleitura.png"
-import Logo from "../../Assets/logo.png"
-import { useNavigate } from "react-router-dom"
-import { useState } from "react"
+import React, { useState } from "react";
+import { Container, Form, FormImage, Image, Input, Label, InputBox, ContainerForm, Divi, LogoImg, ContainerFormulario, StyleForm, Botao, InputBoxContainer } from "./styled";
+import ImagemPrincipal from "../../Assets/imageleitura.png";
+import Logo from "../../Assets/logo.png";
+import { useNavigate } from "react-router-dom";
+import { baseUrl } from "../../services/Api";
+import axios from "axios";
 
 function Cadastro() {
-    const navigate = useNavigate()
-    const [firstName, setFirstName] = useState()
-    const [sobrenome, setSobrenome] = useState()
-    const [email, setEmail] = useState()
-    const [telefone, setTelefone] = useState()
-    const [senha, setSenha] = useState()
-    const [confirmSenha, setConfirmSenha] = useState()
-    
-    const goToLogin = () =>{
-        navigate('/login')
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState({
+        firstName: "",
+        sobrenome: "",
+        email: "",
+        telefone: "",
+        senha: "",
+        confirmSenha: ""
+    });
+
+    const goToLogin = () => {
+        navigate('/login');
     }
 
-    const handleSubmit = () =>{
-        console.log(firstName, sobrenome, email, telefone, senha, confirmSenha)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if(formData.senha !== formData.confirmSenha){
+            alert("Senhas não coincidem")
+        } else{
+            axios.post(`${baseUrl}/user/create`, formData)
+              .then(function (response) {
+                alert("Usuário Criado")
+                goToLogin()
+              })
+              .catch(function (error) {
+                alert(error.response.data.message)
+              });
+        }
+
+        setFormData({
+            firstName: "",
+            sobrenome: "",
+            email: "",
+            telefone: "",
+            senha: "",
+            confirmSenha: ""
+        });
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
     }
 
     return (
-        <>
-            <Container>
-                <ContainerForm  >
-                    <Form>
-                        <Divi>
-                            <FormImage>
-                                <Image src={ImagemPrincipal} />
-                            </FormImage>
-                            <ContainerFormulario onSubmit={handleSubmit}>
-                                <LogoImg src={Logo} />
-                                <StyleForm >
-                                    <InputBoxContainer>
-                                        <InputBox>
-                                            <Label for="firstname">Primeiro Nome</Label>
-                                            <Input id="firstname" 
-                                            type="text" 
-                                            name="firstname" 
-                                            value={firstName}
-                                            onChange={(e)=>setFirstName(e.target.value)}
-                                            placeholder="Digite seu primeiro nome" 
-                                            required></Input>
-                                        </InputBox>
-                                        <InputBox>
-                                            <Label for="sobrenome">Sobrenome</Label>
-                                            <Input id="sobrenome" 
-                                            type="text" 
-                                            name="sobrenome" 
-                                            value={sobrenome}
-                                            onChange={(e)=>setSobrenome(e.target.value)}
-                                            placeholder="Digite seu sobrenome" 
-                                            required></Input>
-                                        </InputBox>
-                                        <InputBox>
-                                            <Label for="email">Email</Label>
-                                            <Input id="email" 
-                                            type="text" 
-                                            name="email" 
-                                            value={email}
-                                            onChange={(e)=>setEmail(e.target.value)}
-                                            placeholder="Digite seu email" 
-                                            required></Input>
-                                        </InputBox>
-                                    </InputBoxContainer>
-                                    <InputBoxContainer>
-                                        <InputBox>
-                                            <Label for="telefone">Telefone</Label>
-                                            <Input id="telefone" 
-                                            type="tel" 
+        <Container>
+            <ContainerForm>
+                <Form>
+                    <Divi>
+                        <FormImage>
+                            <Image src={ImagemPrincipal} alt="Imagem Principal" />
+                        </FormImage>
+                        <ContainerFormulario onSubmit={handleSubmit}>
+                            <LogoImg src={Logo} alt="Logo" />
+                            <StyleForm>
+                                <InputBoxContainer>
+                                    <InputBox>
+                                        <Label htmlFor="firstName">Primeiro Nome</Label>
+                                        <Input
+                                            id="firstName"
+                                            type="text"
+                                            name="firstName"
+                                            value={formData.firstName}
+                                            onChange={handleChange}
+                                            placeholder="Digite seu primeiro nome"
+                                            required
+                                        />
+                                    </InputBox>
+                                    <InputBox>
+                                        <Label htmlFor="sobrenome">Sobrenome</Label>
+                                        <Input
+                                            id="sobrenome"
+                                            type="text"
+                                            name="sobrenome"
+                                            value={formData.sobrenome}
+                                            onChange={handleChange}
+                                            placeholder="Digite seu sobrenome"
+                                            required
+                                        />
+                                    </InputBox>
+                                    <InputBox>
+                                        <Label htmlFor="email">Email</Label>
+                                        <Input
+                                            id="email"
+                                            type="text"
+                                            name="email"
+                                            value={formData.email}
+                                            onChange={handleChange}
+                                            placeholder="Digite seu email"
+                                            required
+                                        />
+                                    </InputBox>
+                                </InputBoxContainer>
+                                <InputBoxContainer>
+                                    <InputBox>
+                                        <Label htmlFor="telefone">Telefone</Label>
+                                        <Input
+                                            id="telefone"
+                                            type="tel"
                                             name="telefone"
-                                            value={telefone}
-                                            onChange={(e)=>setTelefone(e.target.value)} 
-                                            placeholder="Digite seu telefone" 
-                                            required></Input>
-                                        </InputBox>
-                                        <InputBox>
-                                            <Label for="senha">Senha</Label>
-                                            <Input id="senha" 
-                                            type="password" 
+                                            value={formData.telefone}
+                                            onChange={handleChange}
+                                            placeholder="Digite seu telefone"
+                                            required
+                                        />
+                                    </InputBox>
+                                    <InputBox>
+                                        <Label htmlFor="senha">Senha</Label>
+                                        <Input
+                                            id="senha"
+                                            type="password"
                                             name="senha"
-                                            value={senha}
-                                            onChange={(e)=>setSenha(e.target.value)} 
-                                            placeholder="Digite sua senha" 
-                                            required></Input>
-                                        </InputBox>
-                                        <InputBox>
-                                            <Label for="confirmsenha">Confirme sua senha</Label>
-                                            <Input id="confirmsenha" 
-                                            type="password" 
-                                            value={confirmSenha}
-                                            onChange={(e)=>setConfirmSenha(e.target.value)}
-                                            name="confirmsenha" 
-                                            placeholder="Confirme sua senha" 
-                                            required></Input>
-                                        </InputBox>
-                                    </InputBoxContainer>
-                                </StyleForm>
-                                <Botao type="submit">Continuar</Botao>
-                            </ContainerFormulario>
-                        </Divi>
-                    </Form>
-                </ContainerForm >
-            </Container>
-
-        </>
-    )
+                                            value={formData.senha}
+                                            onChange={handleChange}
+                                            placeholder="Digite sua senha"
+                                            required
+                                        />
+                                    </InputBox>
+                                    <InputBox>
+                                        <Label htmlFor="confirmSenha">Confirme sua senha</Label>
+                                        <Input
+                                            id="confirmSenha"
+                                            type="password"
+                                            name="confirmSenha"
+                                            value={formData.confirmSenha}
+                                            onChange={handleChange}
+                                            placeholder="Confirme sua senha"
+                                            required
+                                        />
+                                    </InputBox>
+                                </InputBoxContainer>
+                            </StyleForm>
+                            <Botao type="submit">Cadastrar</Botao>
+                        </ContainerFormulario>
+                    </Divi>
+                </Form>
+            </ContainerForm>
+        </Container>
+    );
 }
 
-export default Cadastro
+export default Cadastro;
